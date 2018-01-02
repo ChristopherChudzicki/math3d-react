c7f7f8abc7fbc9fbb1702e52d0af741c1ee4d0c5
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-01-02 10:29:56 -0500

    add tests for findClosingBrace

--- src/utils/helpers.test.js
+++ src/utils/helpers.test.js
@@ -6,8 +6,9 @@ import {
 from './helpers'
 
 test('escaping regular expressions', () => {
-  const escaped = escapeRegExp('3 * sin(x)^2')
-  expect(escaped).toBe('3 \\* sin\\(x\\)\\^2')
+  const input = '3 * sin(x)^2'
+  const goal  = '3 \\* sin\\(x\\)\\^2'
+  expect(escapeRegExp(input)).toBe(goal)
 })
 
 describe('replaceAll', () => {
@@ -25,5 +26,32 @@ describe('replaceAll', () => {
 })
 
 describe('findClosingBrace', () => {
-  // TODO
+  test('finding a closing brace', () => {
+
+    //                  01234567890123456789012345678901234567890123456789
+    const expression = '4 + ( sin( 3^(1) - 7^2 ) + 5 ) + exp(8 - (4*3) )'
+    const startIdx = 4
+    expect(findClosingBrace(expression, startIdx)).toBe(29)
+
+  })
+
+  test('throws an error if no opening brace at specified location', () => {
+    //                  0123456789
+    const expression = '4 + sin(x)'
+    const startIdx = 2
+    const badfunc = () =>  findClosingBrace(expression, startIdx)
+    expect(badfunc).toThrow(
+      `${expression} does not contain an opening brace at position ${startIdx}.`
+    )
+  })
+
+  test('throws an error if brace opens but does not close', () => {
+    //                  012345678901234
+    const expression = '4 + sin( e^(2t)'
+    const startIdx = 7
+    const badfunc = () =>  findClosingBrace(expression, startIdx)
+    expect(badfunc).toThrow(
+      `${expression} has a brace that opens at position ${startIdx} but does not close.`
+    )
+  })
 })
