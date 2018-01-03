30555350ed3bb2562e597e3607df595a8f977448
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-01-02 20:47:48 -0500

    add variable detection to ParsedExpression

--- src/utils/math/ParsedExpression.js
+++ src/utils/math/ParsedExpression.js
@@ -1,5 +1,45 @@
 import math from 'mathjs'
 
+/**
+ * Uses math.parse to parse a math expression into a tree. Holds the tree and
+ * some helper information (e.g., variable and function dependencies) and
+ * provides some helper methods.
+ */
 export default class ParsedExpression {
+
+  parseTree = null
+  error = null
+  // these store dependencies required before evaluation
+  functionsUsed = null
+  variablesUsed = null
+
+  constructor(string) {
+    try {
+      this.parseTree = math.parse(string)
+      const { variablesUsed, functionsUsed } = this.getDependencies()
+      this.variablesUsed = variablesUsed
+      this.functionsUsed = functionsUsed
+    }
+    catch (error) {
+      this.error = error
+      throw (error)
+    }
+  }
+
+  getDependencies() {
+    const variablesUsed = []
+    const functionsUsed = []
+
+    this.parseTree.traverse(node => {
+      if (node.type === 'SymbolNode') {
+        variablesUsed.push(node.name)
+      }
+      else if (node.type === 'FunctionNode') {
+        functionsUsed.push(node.name)
+      }
+    } )
+
+    return { variablesUsed, functionsUsed }
+  }
 
 }
--- src/utils/math/ParsedExpression.test.js
+++ src/utils/math/ParsedExpression.test.js
@@ -1,13 +1,19 @@
 import ParsedExpression from './ParsedExpression'
 
 describe('ParsedExpression', () => {
+  test('detects variables and function dependencies', () => {
+    const expression = 'f(4x, b*y, g(z)) + sin(t^2) - sqrt(4*B)'
+    const variablesUsed = ['B', 't', 'z', 'y', 'b', 'x']
+    const functionsUsed = ['f', 'g', 'sin', 'sqrt']
+    const parsed = new ParsedExpression(expression)
 
-  // Describe dependency detection
+    expect(parsed.variablesUsed.sort()).toEqual(variablesUsed.sort())
+    expect(parsed.functionsUsed.sort()).toEqual(functionsUsed.sort())
+  } )
 
   // Describe operator replacement & test with evaluation
 
   // Describe preprocess & test with evaluation
 
   // Describe error handling
-
 } )
