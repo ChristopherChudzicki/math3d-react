a5728c8a01efb95d60953d5665ae6b287758b6d1
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-01-02 20:15:39 -0500

    reformat some docs

--- src/utils/math/preprocessHOFs.js
+++ src/utils/math/preprocessHOFs.js
@@ -1,21 +1,25 @@
 import { escapeRegExp, replaceAll, findClosingBrace } from '../helpers'
 
 /**
- * A preprocessor to be used before mathjs's math.parse function.
- * Motivation:
- *  For higher-order functions (HOFs) like differentiation, we want to write
- *  diff(f)(x), where diff(f) evaluates to a function which is subsequently
- *  evaluated at value x.
+ * A preprocessor to be used before mathjs's math.parse function. Motivation:
+ * For higher-order functions (HOFs) like differentiation, we want to write
+ * diff(f)(x), where diff(f) evaluates to a function which is subsequently
+ * evaluated at value x.
  *
- *  But mathjs's parser can't handle this. It interprets diff(f)(x) as two
- *  numbers, diff(f) and (x), implicitly multiplied. The alternative we've
- *  settled for is expressions like diff(f, x).
+ * But mathjs's parser can't handle this. It interprets diff(f)(x) as two
+ * numbers, diff(f) and (x), implicitly multiplied. The alternative we've
+ * settled for is expressions like diff(f, x).
  *
- *  So this preprocessor turns user-facing syntax to mathjs-facing syntax:
- *    diff(f)(u,v) --> diff(f, u, v)
- *  Note that diff(f) without a subsequent evaluation remains unchanged,
- *  which can show up second derivatives:
- *    diff( diff(f) )(u, v) --> diff( diff(f), u, v)
+ * So this preprocessor turns user-facing syntax to mathjs-facing syntax:
+ *
+ * diff(f)(u,v) --> diff(f, u, v)
+ *
+ * Note that diff(f) without a subsequent evaluation remains unchanged, which
+ * can show up second derivatives:
+ *
+ * diff( diff(f) )(u, v) --> diff( diff(f), u, v)
+ *
+ * @param  {array} hofNames An array of higher-order function names
  */
 export default function preprocessHOFs(hofNames) {
   return string => hofNames.reduce((acc, c) => convertSingleHOF(acc, c), string)
