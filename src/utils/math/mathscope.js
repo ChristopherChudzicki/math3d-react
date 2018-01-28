d9c14538f73b9fdfa3c63af54ff9d7a261bd4a93
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-01-28 13:35:36 -0500

    use toposort to determine evaluation order

--- src/utils/math/mathscope.js
+++ src/utils/math/mathscope.js
@@ -1,3 +1,35 @@
-export function deserialize(serialized, parserCache) {
+import toposort from 'toposort'
+
+export function genMathScope(symbols, parserCache) {
+  // Get the evaluation order
+  // add symbols to scope
+  //  - be careful with functions
+
+  getEvalOrder(symbols, parserCache)
+
   return {}
+}
+
+export function getEvalOrder(symbols, parserCache) {
+  // construct dependency graph as array of nodes
+
+  const graph = Object.keys(symbols).reduce(
+    (nodes, symbolName) => {
+      const symbol = symbols[symbolName]
+
+      // Add a new node for each dependency
+      const newNodes = parserCache.getParsed(symbol.expression).dependencies
+        // Except not for function arguments
+        .filter(
+          dependency => !symbol.isFunction || !symbol.arguments.includes(dependency)
+        )
+        .map(
+          dependency => [dependency, symbolName]
+        )
+
+      return [...nodes, ...newNodes]
+    }, []
+  )
+
+  return toposort(graph)
 }
--- src/utils/math/mathscope.test.js
+++ src/utils/math/mathscope.test.js
@@ -1,10 +1,49 @@
-import { deserialize } from './mathscope'
+import { deserialize, getEvalOrder } from './mathscope'
 import ParserCache from './ParserCache'
 
 const DIGITS = 6
 
 describe('deserializing mathscope', () => {
-  test('dependencies resolve correctly when no errors present', () => {
+  test('dependencies are sorted correctly', () => {
+    const symbols = {
+      a: {
+        expression: 'b + c'
+      },
+      b: {
+        expression: '2'
+      },
+      c: {
+        expression: 'b + f(3)'
+      },
+      f: {
+        expression: 'b\\cdot x^2',
+        isFunction: true,
+        arguments: ['x']
+      }
+    }
+    const parserCache = new ParserCache()
+
+    expect(getEvalOrder(symbols, parserCache)).toEqual( ['b', 'f', 'c', 'a'] )
+  } )
+
+  test('cyclic dependencies raises error', () => {
+    const symbols = {
+      a: {
+        expression: 'b + 1'
+      },
+      b: {
+        expression: 'c + 1'
+      },
+      c: {
+        expression: 'a + 1'
+      }
+    }
+    const parserCache = new ParserCache()
+
+    expect(() => getEvalOrder(symbols, parserCache)).toThrow('Cyclic dependency: "c"')
+  } )
+
+  test('scope generated correctly when no errors present', () => {
     // scope
     // a = b/2 - c
     // f(x, y) = a * x^2 - b * y
@@ -22,35 +61,31 @@ describe('deserializing mathscope', () => {
       g: t => t ** 3 - 1
     }
 
-    const serializedScope = {
-      id0: {
-        name: 'a',
+    const symbols = {
+      a: {
         expression: '\\frac{b}{2}-c'
       },
-      id1: {
-        name: 'f\\left(x,y\\right)',
-        expression: ''
+      f: {
+        arguments: ['x', 'y'],
+        expression: 'a\\cdot x^2-b\\cdot y'
       },
-      id2: {
-        name: 'b',
+      b: {
         expression: 'g\\left(4\\right)'
       },
-      id3: {
-        name: 'g\\left(t\\right)',
+      g: {
+        arguments: ['t'],
         expression: 't^{2+d}+c'
       },
-      id4: {
-        name: 'c',
+      c: {
         expression: '-1'
       },
-      id5: {
-        name: 'd',
+      d: {
         expression: '1'
       }
     }
 
     const parserCache = new ParserCache()
-    const mathScope = deserialize(serializedScope, parserCache)
+    const mathScope = deserialize(symbols, parserCache)
 
     expect(mathScope.a).toBeCloseTo(expectedScope.a, DIGITS)
     expect(mathScope.b).toBeCloseTo(expectedScope.b, DIGITS)
