b122841858a4ce0f9326cce1ee7bbe6976c229ab
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-03-06 13:52:31 -0500

    restore isCollapsed randomness in mockData

--- src/mockData.js
+++ src/mockData.js
@@ -12,7 +12,7 @@ function addFolder(store) {
   store.sortableTree[id] = []
   store.folders[id] = {
     title: `Folder ${id}`,
-    isCollapsed: false
+    isCollapsed: Math.random() > 0.5
   }
 
   return id
