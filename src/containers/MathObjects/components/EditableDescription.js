1d6d5fd567c2a9c12cd0be752cdc43262020bba7
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-03-23 13:54:34 -0400

    automatically size EditableDescription's width

--- src/containers/MathObjects/components/EditableDescription.js
+++ src/containers/MathObjects/components/EditableDescription.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 // ant design also has an autosizing textarea, but I found it too hard to resize
 import Textarea from 'react-textarea-autosize'
 import PropTypes from 'prop-types'
 
 const StyledTextarea = styled(Textarea)`
-  width: 700px;
+  width: ${props => props.width};
   max-width: calc(100% - 35px);
   resize: none;
   border-top:none;
@@ -14,6 +14,8 @@ const StyledTextarea = styled(Textarea)`
   border-radius: 0px;
   padding-bottom: 0px;
   padding-top: 0px;
+  padding-right:2px;
+  padding-left:2px;
   border-bottom: 1px solid ${props => props.theme.medium};
   color: ${props => props.theme.dark};
   &:focus {
@@ -23,22 +25,55 @@ const StyledTextarea = styled(Textarea)`
   }
 `
 
-EditableDescription.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+export default class EditableDescription extends PureComponent {
+
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+  }
+
+  state = {
+    width: '100%'
+  }
+
+  componentDidMount() {
+    this.setWidth(this.props.value)
+  }
+
+  setWidth = text => {
+    const textWidth = getTextWidth(text, '14px sans-serif')
+    const paddingWidth = 4
+
+    this.setState( {
+      width: `${textWidth + paddingWidth}px`
+    } )
+  }
+
+  onChange = e => {
+    const text = e.target.value
+    this.setWidth(text)
+    this.props.onChange(text)
+  }
+
+  render() {
+    return (
+      <StyledTextarea
+        rows={1}
+        width={this.state.width}
+        value={this.props.value}
+        onChange={this.onChange}
+      />
+    )
+  }
+
 }
 
-export default function EditableDescription(props) {
-  return (
-    <StyledTextarea
-      rows={1}
-      value={props.value}
-      onChange={e => props.onChange(e.target.value)}
-    />
-  )
+function getTextWidth(text, font) {
+  // re-use canvas object for better performance
+  const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement('canvas'))
+  const context = canvas.getContext('2d')
+  context.font = font
+  const metrics = context.measureText(text)
+  const roundedWidth = Math.floor(metrics.width) + 1
+  return roundedWidth
 }
-
-// TODO: Currently we autosize the height only. It would be nice to autosize the
-// width also. Possibly using this method:
-// https://stackoverflow.com/a/21015393/2747370
-// would need to cap at max-width at some point.
