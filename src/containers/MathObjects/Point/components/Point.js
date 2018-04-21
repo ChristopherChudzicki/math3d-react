d390cc47a48fc13ecc653d2db11cd7ba1fdaef61
Jason Chen <kbtpodifo@gmail.com>
2018-04-21 16:49:27 -0400

    Add StatusSymbol to Point component with props

--- src/containers/MathObjects/Point/components/Point.js
+++ src/containers/MathObjects/Point/components/Point.js
@@ -43,7 +43,11 @@ export default class Point extends PureComponent {
   static propTypes = {
     id: PropTypes.string.isRequired,
     coords: PropTypes.string.isRequired, // latex
-    onEditCoords: PropTypes.func.isRequired
+    color: PropTypes.string.isRequired,
+    visible: PropTypes.bool.isRequired,
+    onEditCoords: PropTypes.func.isRequired,
+    onToggleVisibility: PropTypes.func.isRequired,
+    onSetColor: PropTypes.func.isRequired
   }
 
   onEditCoords = (mq) => {
