8adf29859ac38631f83d073cc382782ae721df1d
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-04-27 16:44:58 -0400

    add ScrollWithOverflow component

---
+++ src/components/ScrollWithOverflow/index.js
@@ -0,0 +1,78 @@
+import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+/**
+ * ScrollWithOverflow is a component intended to allow scrolling in the
+ * y-direction and overflow in the x-direction.
+ *
+ * Naively, this would just be achieved by setting
+ *    overflow-x: visible;
+ *    overflow-y: scroll;
+ * on whatever div you want. But that doesn't work:
+ *    https://stackoverflow.com/questions/6421966/css-overflow-x-visible-and-overflow-y-hidden-causing-scrollbar-issue
+ * This component solves the problem by adding positive padding and
+ * negative margin:
+ *  padding-right: 100vw;
+ *  margin-right: -100vw;
+ * so that children can appear to overflow without actually overflowing.
+ *
+ * This approach has a few downsides:
+ *  1. Other elements in the padding region do not receive pointer events.
+ *    - solved by conditional application of pointer-events:none
+ *  2. The scrollbar disappears (pushed by padding)
+ *    - TODO: Solve this, or at least alleviate with other styling (like
+ *      top/bottom shadows) to indicate scrolling
+ * Not ideal, but the best I've got for now...
+ */
+
+const ScrollingDiv = styled.div`
+  overflow-y: scroll;
+  padding-right: 100vw;
+  margin-right: -100vw;
+  height: calc(100% - 50px);
+  pointer-events: ${props => props.disablePointer ? 'none' : 'auto'};
+`
+
+const ScrollingDivInner = styled.div`
+  overflow-x:visible;
+  pointer-events:auto;
+  height:100%;
+`
+
+export default class ScrollWithOverflow extends PureComponent {
+
+  static propTypes = {
+    children: PropTypes.oneOfType( [
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ] )
+  }
+
+  state = {
+    isHovering: false
+  }
+
+  onMouseEnter = () => {
+    this.setState( { isHovering: true } )
+  }
+  onMouseLeave = () => {
+    this.setState( { isHovering: false } )
+  }
+
+  render() {
+    return (
+      <ScrollingDiv
+        disablePointer={!this.state.isHovering}
+      >
+        <ScrollingDivInner
+          onMouseEnter={this.onMouseEnter}
+          onMouseLeave={this.onMouseLeave}
+        >
+          {this.props.children}
+        </ScrollingDivInner>
+      </ScrollingDiv>
+    )
+  }
+
+}
