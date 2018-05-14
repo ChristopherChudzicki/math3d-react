29d44ebf75366bcd613083e5ac27b0deab42e421
Jason Chen <kbtpodifo@gmail.com>
2018-05-13 23:36:45 -0400

    Rewrite LongPressable dragThreshold as distance
    Also cancel long press if pointer leaves element.

--- src/components/LongPressable/index.js
+++ src/components/LongPressable/index.js
@@ -2,6 +2,19 @@ import React from 'react'
 import PropType from 'prop-types'
 import Pointable from 'react-pointable'
 
+function eventToPosition(event) {
+  return {
+    x: event.clientX,
+    y: event.clientY
+  }
+}
+
+function distance(pointA, pointB) {
+  return Math.sqrt(
+    Math.pow(pointB.x - pointA.x, 2) + Math.pow(pointB.y - pointA.y, 2)
+  )
+}
+
 export default class LongPressable extends React.PureComponent {
 
   static propTypes = {
@@ -9,6 +22,8 @@ export default class LongPressable extends React.PureComponent {
     onShortPress: PropType.func,
     longPressTime: PropType.number,
     primaryMouseButtonOnly: PropType.bool,
+    // Maximum distance (pixels) user is allowed to drag before
+    // click is canceled
     dragThreshold: PropType.number,
     children: PropType.node
   }
@@ -16,18 +31,21 @@ export default class LongPressable extends React.PureComponent {
   static defaultProps = {
     longPressTime: 500,
     primaryMouseButtonOnly: true,
-    dragThreshold: 0
+    dragThreshold: 100
   }
 
   isLongPressing = false
-  dragCounter = 0
+  startingPosition = { x: 0, y: 0 }
 
-  onPointerUp = () => {
+  onPointerUp = (e) => {
     if (this.timerID) {
       this.clearLongPressTimer()
     }
 
-    if (!this.isLongPressing) {
+    const mousePosition = eventToPosition(e)
+
+    if (!this.isLongPressing &&
+        !this.exceedDragThreshold(mousePosition)) {
       this.props.onShortPress()
     }
     else {
@@ -42,7 +60,7 @@ export default class LongPressable extends React.PureComponent {
       }
     }
 
-    this.dragCounter = 0
+    this.startingPosition = eventToPosition(e)
 
     this.timerID = setTimeout(() => {
       this.isLongPressing = true
@@ -50,10 +68,15 @@ export default class LongPressable extends React.PureComponent {
     }, this.props.longPressTime)
   }
 
-  onPointerMove = () => {
-    this.dragCounter++
+  onPointerMove = (e) => {
+    const mousePosition = eventToPosition(e)
+    if (this.timerID && this.exceedDragThreshold(mousePosition)) {
+      this.clearLongPressTimer()
+    }
+  }
 
-    if (this.timerID && this.dragCounter > this.props.dragThreshold) {
+  onPointerLeave = () => {
+    if (this.timerID) {
       this.clearLongPressTimer()
     }
   }
@@ -63,12 +86,17 @@ export default class LongPressable extends React.PureComponent {
     this.timerID = null
   }
 
+  exceedDragThreshold(point) {
+    return distance(this.startingPosition, point) > this.props.dragThreshold
+  }
+
   render() {
     return (
       <Pointable
         onPointerUp={this.onPointerUp}
         onPointerDown={this.onPointerDown}
         onPointerMove={this.onPointerMove}
+        onPointerLeave={this.onPointerLeave}
       >
         {this.props.children}
       </Pointable>
