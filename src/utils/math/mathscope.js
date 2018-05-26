c344cab977c44586d4b61472c60ee920d55d0f5a
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-05-26 17:46:03 -0400

    refactor mathscope & tests

--- src/utils/math/mathscope.js
+++ src/utils/math/mathscope.js
@@ -1,25 +1,25 @@
 import toposort from 'toposort'
+import diff from 'shallow-diff'
 
 const DEFAULT_SCOPE = {}
 
 /**
- * @module mathscope
  * Functions for evaluating a serialized description of mathematical symbols.
  * This includes sorting the symbols into a valid evaluation order.
  *
- * As an example, consider the serialized "mathscope" below:
+ * As an example, consider the serialized symbol:values 'scope' below:
  *
  *     a = b/2 - c
- *     b = h(4) + c
+ *     b = g(4) + c
  *     c = -1
  *     d = +1
  *     f(x, y) = a*x^2 - b*y
- *     h(t) = t^2 -1
+ *     g(t) = t^3 - 1
  *     p = c^2 + d^2
+ *     z = 1
  *
- * This scope of mathematical symbols forms a directed graph
- * where parents represent evaluation dependencies and must be
- * evaluated first.
+ * These symbol definitions forms a directed graph where parents represent
+ * evaluation dependencies and must be evaluated first.
  *
  * Note that 'x', 'y', and 't' are function arguments and do not appear in our
  * dependency graph.
@@ -28,39 +28,54 @@ const DEFAULT_SCOPE = {}
  *          /
  * c -->-------\
  *        \     -->-- a -->--- f
- * h -->-- b --/              /
+ * g -->-- b --/              /
  *         \                 /
  *         \-------->-------
  *
+ * z        (isolated node)
+ *
  * This module has two main responsibilities:
- *   1. Generate an initial mathscope from a serialized description.
- *   2. Update an exisiting mathscope when a symbol value has changed.
- *      When a single variable changes, we do not need to re-evaluate the entire
- *      mathscope. In the example above, if 'b' changes from 'g(4)' to
- *      'g(3) + p' we only need to update the descendants of g.
+ *   1. Generate an initial symbol:value mapping from a serialized description.
+ *   2. Update an exisiting symbol:value mapping when some variable values have
+ *      changed. For example, when a single variable value changes, we do not
+ *      need to re-evaluate the entire mathscope. In the example above, if 'b'
+ *      changes from 'g(4)' to 'g(3) + p' we only need to update b and its
+ *      descendants.
+ *
+ *  Low-level functions are combined in the MathScope class, which provides
+ *  caching for faster re-computation.
  */
 
 /**
- * Generates an object mapping symbol names to symbol values
+ * evaluates a serialized scope from scratch or updates an existing scope
  *
+ * @param  {Parser} parser for evaluating mathematical expressions
  * @param  {object} symbols
- * @param  {string} symbols[symbolName] An assignment expression, e.g., a = b^2 or f(r, q) = r*sin(q)
- * @param  {Parser} parser
+ * @param  {string} symbols[symbolName] An assignment expression,e.g.,
+ *         a = b^2 or f(r, q) = r*sin(q)
+ * @param  {?object} oldScope
+ * @param  {number | function} oldScope[symbolName] a partial initial scope,
+ *         e.g., { a: 4, b: -2, f: x => x**2 }
+ * @param {?array | set} changed which serialized symbol values have changed.
+ *  Starting from oldScope, only update these symbols and their children
+ *
+ * Note: The optional arguments oldScope and changed should NOT be used if
+ * deleting symbols from or adding symbols to the scope. These arguments are
+ * intended to improve performance when changing a single symbol's very often,
+ * e.g., when the symbol's value is changed by a slider
  */
-export function updateMathScope(symbols, parser, initialScope, updateChildrenOf) {
-  // Get the evaluation order
-  // add symbols to scope
-
-  const evalOrder = getEvalOrder(symbols, parser, updateChildrenOf)
+export function evalScope(parser, symbols, oldScope = DEFAULT_SCOPE, changed = null) {
+  // Get the evaluation order and add symbols to scope
+  const evalOrder = getEvalOrder(symbols, parser, changed)
   const initial = {
-    mathScope: { ...initialScope }, // copy initialScope, not mutate
+    scope: { ...oldScope }, // copy oldScope, not mutate
     errors: {},
-    updated: evalOrder
+    updated: new Set(evalOrder)
   }
 
   return evalOrder.reduce((acc, symbolName) => {
     try {
-      acc.mathScope[symbolName] = parser.parse(symbols[symbolName] ).eval(acc.mathScope)
+      acc.scope[symbolName] = parser.parse(symbols[symbolName] ).eval(acc.scope)
     }
     catch (err) {
       acc.errors[symbolName] = err
@@ -69,46 +84,41 @@ export function updateMathScope(symbols, parser, initialScope, updateChildrenOf)
   }, initial)
 
 }
-export function genMathScope(symbols, parser) {
-  return updateMathScope(symbols, parser, DEFAULT_SCOPE, null)
-}
 
 /**
- * Determines a valid evaluation order for symbols. If onlyChildrenOf is
- * supplied, only returns children of those node.
+ * Determines a valid evaluation order for symbols. If onlyTheseAndChildren is
+ * supplied, only returns those nodes and their children.
  *
  * @param  {object} symbols
  * @param  {string} symbols[symbolName] An assignment expression, e.g., a = b^2 or f(r, q) = r*sin(q)
  * @param  {Parser} parser
- * @param  {?array || Set} onlyChildrenOf
+ * @param  {?array | Set} onlyTheseAndChildren
  *
  * @returns {array} of symbol names, a valid evaluation order for symbols
  */
-export function getEvalOrder(symbols, parser, onlyChildrenOf = null) {
+export function getEvalOrder(symbols, parser, onlyTheseAndChildren = null) {
   // construct dependency graph as array of nodes
   const childMap = getChildMap(symbols, parser)
-  const nodesToInclude = onlyChildrenOf
-    ? [...getDescendants(onlyChildrenOf, childMap)]
+  const nodesToInclude = onlyTheseAndChildren
+    ? [...getDescendants(onlyTheseAndChildren, childMap)]
     : Object.keys(childMap)
 
-  // Sort the nodes with children
-  const edges = nodesToInclude.reduce((acc, node) => {
+  // Sort the non-isolated nodes
+  // Alert! Isolated nodes---nodes without parents or children---are missed.
+  // We'll add them in a moment
+  const { edges, childless } = nodesToInclude.reduce((acc, node) => {
+    if (childMap[node].size === 0) {
+      acc.childless.push(node)
+    }
     for (const child of childMap[node] ) {
-      acc.push( [node, child] )
+      acc.edges.push( [node, child] )
     }
     return acc
-  }, [] )
+  }, { edges: [], childless: [] } )
   const sorted = toposort(edges)
+  const included = new Set(sorted)
 
-  // If the changed node has no children, it will not be included in the sorting
-  // above. So add it now.
-  onlyChildrenOf && onlyChildrenOf.forEach(item => {
-    if (!sorted.includes(item)) {
-      sorted.push(item)
-    }
-  } )
-
-  return sorted
+  return [...sorted, ...childless.filter(node => !included.has(node))]
 }
 
 /**
@@ -177,4 +187,57 @@ function setMergeInto(target, source) {
     target.add(item)
   }
   return target
+}
+
+export class ScopeEvaluator {
+
+  constructor(parser) {
+    this._parser = parser
+  }
+
+  _oldResult = {
+    scope: {},
+    errors: {},
+    updated: {}
+  }
+  _oldSymbols = {}
+
+  evalScope(symbols) {
+
+    if (symbols === this._oldSymbols) {
+      return this._oldResult
+    }
+
+    const { updatesOnly, changed } = ScopeEvaluator._detectChanges(symbols, this._oldSymbols)
+
+    const result = updatesOnly
+      ? this._patchScope(symbols, changed)
+      : this._recalculateScope(symbols)
+
+    this._updateState(symbols, result)
+    return result
+
+  }
+
+  static _detectChanges(newSymbols, oldSymbols) {
+    const { added, deleted, updated } = diff(newSymbols, oldSymbols)
+    return {
+      updatesOnly: added.length === 0 && deleted.length === 0,
+      changed: updated
+    }
+  }
+
+  _patchScope(symbols, changed) {
+    return evalScope(this._parser, symbols, this._oldResult.scope, changed)
+  }
+
+  _recalculateScope(symbols) {
+    return evalScope(this._parser, symbols)
+  }
+
+  _updateState(symbols, result) {
+    this._oldSymbols = symbols
+    this._oldResult = result
+  }
+
 }
--- src/utils/math/mathscope.test.js
+++ src/utils/math/mathscope.test.js
@@ -1,19 +1,24 @@
 import {
-  updateMathScope,
-  genMathScope,
+  evalScope,
   getEvalOrder,
   getChildMap,
   getDescendantsOfNode,
-  getDescendants
+  getDescendants,
+  ScopeEvaluator
 }
   from './mathscope'
+import { toNearlyEqual } from './matchers'
 import Parser from './Parser'
 
-const DIGITS = 6
+// For comparing approximate equality of arrays, functions, objects.
+// For numbers, this uses a default tolerance of 1e-6
+expect.extend( {
+  toNearlyEqual
+} )
 
-describe('deserializing mathscope', () => {
+describe('evalScope', () => {
 
-  test('scope created correctly when no errors present', () => {
+  describe('evaluating a new scope from scratch without errors', () => {
 
     const symbols = {
       a: 'a=\\frac{b}{2}-c',
@@ -22,55 +27,62 @@ describe('deserializing mathscope', () => {
       g: 'g\\left(t\\right)=t^{2+d}+c',
       c: 'c=-1',
       d: 'd=1',
-      w: 'w=(d+1)^2'
+      w: 'w=(d+1)^2',
+      y: 'y=100'
     }
-
     const expectedScope = {
       a: 32.5,
       b: 63,
       c: -1,
       d: 1,
       w: 4,
+      y: 100,
       f: (x, y) => 32.5 * (x ** 2) - 63 * y,
       g: t => t ** 3 - 1
     }
-    const expectedUpdated = ['a', 'b', 'c', 'd', 'w', 'f', 'g']
+
+    const expectedUpdated = new Set(Object.keys(expectedScope))
 
     const parser = new Parser()
-    const { mathScope, updated, errors } = genMathScope(symbols, parser)
+    const { scope, updated, errors } = evalScope(parser, symbols)
 
-    expect(mathScope.a).toBeCloseTo(expectedScope.a, DIGITS)
-    expect(mathScope.b).toBeCloseTo(expectedScope.b, DIGITS)
-    expect(mathScope.c).toBeCloseTo(expectedScope.c, DIGITS)
-    expect(mathScope.d).toBeCloseTo(expectedScope.d, DIGITS)
-    expect(mathScope.w).toBeCloseTo(expectedScope.w, DIGITS)
-    expect(mathScope.f(2, 7)).toBeCloseTo(expectedScope.f(2, 7), DIGITS)
-    expect(mathScope.g(5)).toBeCloseTo(expectedScope.g(5), DIGITS)
+    test("scope's symbol evaluations are correct", () => {
+      expect(scope).toNearlyEqual(expectedScope)
+    } )
 
-    expect(updated.sort()).toEqual(expectedUpdated.sort())
-    expect(errors).toEqual( {} )
+    test('updated symbols recorded accurately', () => {
+      expect(updated).toEqual(expectedUpdated)
+    } )
+
+    test('No errors created', () => {
+      expect(errors).toEqual( {} )
+    } )
+
   } )
 
-  test('updating an existing mathscope', () => {
+  describe('updating an existing scope evaluation', () => {
     const newSymbols = {
       a: 'a=\\frac{b}{2}-c',
       f: 'f\\left(x,y\\right)=a\\cdot x^2-b\\cdot y',
-      b: 'b=g\\left(5\\right)',
+      b: 'b=g\\left(5\\right)', // changed
       g: 'g\\left(t\\right)=t^{2+d}+c',
       c: 'c=-1',
       d: 'd=1',
-      w: 'w=(d+1)^3'
+      w: 'w=(d+1)^3', // changed
+      y: 'y=150' // changed
     }
 
-    const initialScope = {
+    const oldScope = {
       a: 32.5,
       b: 63,
       c: -1,
       d: 1,
       w: 4,
+      y: 100,
       f: (x, y) => 32.5 * (x ** 2) - 63 * y,
       g: t => t ** 3 - 1
     }
+    const changed = ['b', 'w', 'y']
 
     const expectedScope = {
       a: 63,
@@ -78,50 +90,56 @@ describe('deserializing mathscope', () => {
       c: -1,
       d: 1,
       w: 8,
+      y: 150,
       f: (x, y) => 63 * (x ** 2) - 124 * y,
       g: t => t ** 3 - 1
     }
-    const expectedUpdated = ['a', 'b', 'f', 'w']
+    const expectedUpdated = new Set( ['a', 'b', 'f', 'w', 'y'] )
 
     const parser = new Parser()
     const {
-      mathScope,
+      scope,
       updated,
       errors
-    } = updateMathScope(newSymbols, parser, initialScope, ['b', 'w'] )
+    } = evalScope(parser, newSymbols, oldScope, changed)
 
-    // mathscope should be a new object, not the same as initialScope
-    expect(mathScope).not.toBe(initialScope)
+    test('updating scope creates a new object', () => {
+      expect(scope).not.toBe(oldScope)
+    } )
 
-    // mathscope should behave correctly
-    expect(mathScope.a).toBeCloseTo(expectedScope.a, DIGITS)
-    expect(mathScope.b).toBeCloseTo(expectedScope.b, DIGITS)
-    expect(mathScope.c).toBeCloseTo(expectedScope.c, DIGITS)
-    expect(mathScope.d).toBeCloseTo(expectedScope.d, DIGITS)
-    expect(mathScope.w).toBeCloseTo(expectedScope.w, DIGITS)
-    expect(mathScope.f(2, 7)).toBeCloseTo(expectedScope.f(2, 7), DIGITS)
-    expect(mathScope.g(5)).toBeCloseTo(expectedScope.g(5), DIGITS)
+    test("scope's symbol evaluations are correct", () => {
+      expect(updated).toEqual(expectedUpdated)
+    } )
 
-    // updated list should be correct
-    expect(updated.sort()).toEqual(expectedUpdated.sort())
+    test('updated symbols recorded accurately', () => {
+      expect(updated).toEqual(expectedUpdated)
+    } )
 
-    // no errors
-    expect(errors).toEqual( {} )
+    test('No errors created', () => {
+      expect(errors).toEqual( {} )
+    } )
+
   } )
 
-  test('errors get stored', () => {
+  describe('storing errors', () => {
+
     const symbols = {
       a: '[1, 2, 3]',
       b: '2^a'
     }
 
     const parser = new Parser()
-    const { mathScope, errors } = genMathScope(symbols, parser)
+    const { scope, errors } = evalScope(parser, symbols)
 
-    expect(mathScope.a).toEqual( [1, 2, 3] )
-    expect(errors.b).toBeInstanceOf(Error)
-    expect(() => { throw errors.b } )
-      .toThrow('Unexpected type of argument')
+    test('Error is created and stored', () => {
+      expect(errors.b).toBeInstanceOf(Error)
+      expect(() => { throw errors.b } )
+        .toThrow('Unexpected type of argument')
+    } )
+
+    test('Unrelated symbols are evaluated correctly', () => {
+      expect(scope.a).toEqual( [1, 2, 3] )
+    } )
 
   } )
 
@@ -159,6 +177,7 @@ describe('generating evaluation order', () => {
     h: 'h\\left(t\\right)=t^{2}-1',
     c: '-1',
     d: '1',
+    z: '100', // isolated node, no parents or children
     p: 'c^2+d^2'
   }
 
@@ -172,7 +191,8 @@ describe('generating evaluation order', () => {
       d: new Set( ['p'] ),
       f: new Set(),
       h: new Set( ['b'] ),
-      p: new Set()
+      p: new Set(),
+      z: new Set()
     }
 
     const actualChildMap = getChildMap(symbols, parser)
@@ -196,7 +216,7 @@ describe('generating evaluation order', () => {
       new Set( ['b', 'a', 'f', 'a2'] )
     )
 
-    expect(getDescendants(['b', 'd'], childMap)).toEqual(
+    expect(getDescendants( ['b', 'd'], childMap)).toEqual(
       new Set( ['b', 'a', 'f', 'a2', 'd', 'p'] )
     )
 
@@ -206,7 +226,7 @@ describe('generating evaluation order', () => {
     const parser = new Parser()
     const evalOrder = getEvalOrder(symbols, parser)
     // This is a valid order. there are other valid orders, too
-    const expected = ['h', 'c', 'b', 'a', 'a2', 'f', 'd', 'p']
+    const expected = ['h', 'c', 'b', 'a', 'a2', 'f', 'd', 'p', 'z']
 
     expect(evalOrder).toEqual(expected)
   } )
@@ -230,6 +250,90 @@ describe('generating evaluation order', () => {
     const parser = new Parser()
 
     expect(() => getEvalOrder(cyclicSymbols, parser)).toThrow('Cyclic dependency:')
+  } )
+
+} )
+
+describe('class ScopeEvaluator', () => {
+
+  const symbols0 = {
+    a: 'a=[4,-2,b]',
+    b: 'b=f(2)',
+    f: 'f(t)=t^3',
+    w: 'w=-15'
+  }
+  const expectedScope0 = {
+    a: [4, -2, 8],
+    b: 8,
+    f: t => t**3,
+    w: -15
+  }
+
+  const symbols1 = {
+    a: 'a=[4,-2,b]', // parent changed
+    b: 'b=f(3)', // direct change
+    f: 'f(t)=t^3',
+    w: 'w=-15'
+  }
+  const expectedScope1 = {
+    a: [4, -2, 27],
+    b: 27,
+    f: t => t**3,
+    w: -15
+  }
+
+  test('initial evaluation is correct', () => {
+    const parser = new Parser()
+    const scopeEvaluator = new ScopeEvaluator(parser)
+
+    const { scope } = scopeEvaluator.evalScope(symbols0)
+    expect(scope).toNearlyEqual(expectedScope0)
+  } )
+
+  test('provides cached result if symbols are identical', () => {
+    const parser = new Parser()
+    const scopeEvaluator = new ScopeEvaluator(parser)
+    const recalculateScope = jest.spyOn(scopeEvaluator, '_recalculateScope')
+
+    expect(recalculateScope).toHaveBeenCalledTimes(0)
+    const { firstEval } = scopeEvaluator.evalScope(symbols0)
+    const { secondEval } = scopeEvaluator.evalScope(symbols0)
+    expect(recalculateScope).toHaveBeenCalledTimes(1)
+    expect(firstEval).toBe(secondEval)
+  } )
+
+  test('when symbols are updated but not added/removed, scope is patched', () => {
+    const parser = new Parser()
+    const scopeEvaluator = new ScopeEvaluator(parser)
+    const recalculateScope = jest.spyOn(scopeEvaluator, '_recalculateScope')
+    const patchScope = jest.spyOn(scopeEvaluator, '_patchScope')
+
+    expect(recalculateScope).toHaveBeenCalledTimes(0)
+    expect(patchScope).toHaveBeenCalledTimes(0)
+    scopeEvaluator.evalScope(symbols0)
+    const { scope: patchedScope } = scopeEvaluator.evalScope(symbols1)
+    expect(recalculateScope).toHaveBeenCalledTimes(1)
+    expect(patchScope).toHaveBeenCalledTimes(1)
+    expect(patchedScope).toNearlyEqual(expectedScope1)
+  } )
+
+  test('adding and removing symbols', () => {
+    const parser = new Parser()
+    const scopeEvaluator = new ScopeEvaluator(parser)
+    const recalculateScope = jest.spyOn(scopeEvaluator, '_recalculateScope')
+
+    const symbols = { a: '1', b: '2' }
+    const expectedScope = { a: 1, b: 2 }
+    const addedSymbols = { c: '3', ...symbols }
+    const addedScope = { c: 3, ...expectedScope }
+
+    expect(recalculateScope).toHaveBeenCalledTimes(0)
+    expect(scopeEvaluator.evalScope(symbols).scope).toNearlyEqual(expectedScope)
+    expect(recalculateScope).toHaveBeenCalledTimes(1)
+    expect(scopeEvaluator.evalScope(addedSymbols).scope).toNearlyEqual(addedScope)
+    expect(recalculateScope).toHaveBeenCalledTimes(2)
+    expect(scopeEvaluator.evalScope(symbols).scope).toNearlyEqual(expectedScope)
+
   } )
 
 } )
