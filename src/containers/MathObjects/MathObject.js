f9eef51704c4e385a8d6b41f7a58df1000219bc4
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-06-26 10:05:09 -0400

    memoize onEditDescription

--- src/containers/MathObjects/MathObject.js
+++ src/containers/MathObjects/MathObject.js
@@ -4,6 +4,8 @@ import { setActiveObject } from './services/activeObject/actions'
 import { getParent } from './selectors'
 import { mapTypeToState, FOLDER } from './mathObjectTypes'
 import { deleteMathObject, setProperty } from './actions'
+import createCachedSelector from 're-reselect';
+
 
 const mapStateToProps = (state, ownProps) => {
   const { id, type } = ownProps
@@ -23,14 +25,25 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
+const getOnEditDescription = createCachedSelector(
+  (dispatch, id, type) => dispatch,
+  (dispatch, id, type) => id,
+  (dispatch, id, type) => type,
+  (dispatch, id, type) => (...value) => dispatch(
+    setProperty(id, type, 'description', value)
+  )
+)(
+  (dispatch, id, type) => {
+    return `${type}:${id}`
+  }
+)
+
 const mapDispatchToProps = (dispatch, ownProps) => ( {
   setActiveObject: (id) => dispatch(setActiveObject(id)),
   onDelete: (parentFolderId, positionInParent) => dispatch(
     deleteMathObject(ownProps.id, ownProps.type, parentFolderId, positionInParent)
   ),
-  onEditDescription: (value) => dispatch(
-    setProperty(ownProps.id, ownProps.type, 'description', value)
-  )
+  onEditDescription: getOnEditDescription(dispatch, ownProps.id, ownProps.type)
 } )
 
 export default connect(mapStateToProps, mapDispatchToProps)(MathObject)
