f21b2c2f6375782863214656722f2e2bf2d46314
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-08-10 16:02:43 -0400

    render some mathgraphics

--- src/containers/MathScopeContext/index.js
+++ src/containers/MathScopeContext/index.js
@@ -2,9 +2,9 @@ import { MathScopeProvider } from './components/MathScopeContext'
 import { connect } from 'react-redux'
 import { getParseableSymbols } from './selectors'
 import { setError } from 'services/errors'
+export { MathScopeConsumer } from './components/MathScopeContext'
 
 const mapStateToProps = ( { mathSymbols, sliderValues, parseErrors }, ownProps) => {
-  console.log(mathSymbols)
   const { symbols, idsByName } = getParseableSymbols(ownProps.parser, mathSymbols, sliderValues, parseErrors)
   const evaluationResult = ownProps.scopeEvaluator.evalScope(symbols)
   window.evaluationResult = evaluationResult
--- src/views/Scene.js
+++ src/views/Scene.js
@@ -1,33 +1,78 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import FlexContainer from '../components/FlexContainer'
 import MathBoxContainer from 'containers/MathBoxContainer'
 import MathBox, { Point, Grid, Cartesian } from 'components/MathBox/MathBox'
+import { MathScopeConsumer } from 'containers/MathScopeContext'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { parser } from 'constants/parsing'
+import { setError, EvalErrorData } from 'services/errors'
 
-class Math3dScene extends Component {
+// TODO: This is a connected component. Should we move it into containers?
 
-  state = { z: 0 }
+class Math3dScene extends PureComponent {
 
-  onClick = () => {
-    this.setState( { z: (this.state.z + 1) % 5 } )
+  static propTypes = {
+    order: PropTypes.array.isRequired,
+    mathGraphics: PropTypes.object.isRequired,
+    evalErrors: PropTypes.object.isRequired,
+    setError: PropTypes.func.isRequired
+  }
+
+  evalData(id, data, scope) {
+    const { setError, evalErrors } = this.props
+
+    // TODO: get the toEval array from mathobject type
+    const toEval = ['coords']
+    const evaluated = toEval.reduce((acc, prop) => {
+      try {
+        const parsed = parser.parse(data[prop] )
+        try {
+          acc[prop] = parsed.eval(scope)
+          // clear any old evaluation errors
+          evalErrors[id][prop] && setError(id, prop, new EvalErrorData(null))
+          return acc
+        }
+        catch (evalError) {
+          setError(id, prop, new EvalErrorData(evalError.message))
+          delete acc[prop]
+          return acc
+        }
+      }
+      catch (parseError) {
+        delete acc[prop]
+        return acc
+      }
+
+    }, { ...data } )
+
+    return evaluated
   }
 
   render() {
+    const { mathGraphics, setError } = this.props
     return (
       <FlexContainer style={ { flex: 1 } }
         onClick={this.onClick}
       >
         <MathBoxContainer mathboxElement={window.mathboxElement}>
-          <MathBox mathbox={window.mathbox}>
-            <Cartesian>
-              <Grid axes='xy' />
-              <Grid axes='yz' />
-              <Point
-                coords={[0, 0, this.state.z]}
-                color='blue'
-                size={20}
-              />
-            </Cartesian>
-          </MathBox>
+          <MathScopeConsumer>
+            {( { scope, scopeDiff } ) => {
+              return (
+                <MathBox mathbox={window.mathbox}>
+                  <Cartesian>
+                    <Grid axes='xy' />
+                    <Grid axes='yz' />
+                    {this.props.order.map(id => {
+                      const data = this.evalData(id, mathGraphics[id], scope, setError)
+                      return renderGraphic(id, data)
+                    } )
+                    }
+                  </Cartesian>
+                </MathBox>
+              )
+            }}
+          </MathScopeConsumer>
         </MathBoxContainer>
       </FlexContainer>
     )
@@ -35,4 +80,27 @@ class Math3dScene extends Component {
 
 }
 
-export default Math3dScene
+// TODO: this causes some unnecessary re-renders when tryEval returns an array
+// that is double= but not triple=
+function renderGraphic(id, data) {
+  return (
+    <Point
+      key={id}
+      coords={data.coords}
+      size={20}
+      color={data.color}
+    />
+  )
+}
+
+const mapStateToProps = ( { mathGraphics, evalErrors } ) => ( {
+  mathGraphics,
+  evalErrors,
+  order: Object.keys(mathGraphics)
+} )
+
+const mapDispatchToProps = {
+  setError
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Math3dScene)
