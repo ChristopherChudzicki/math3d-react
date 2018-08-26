14b9a855e54e2d848c2f797d613f6c8c75456c17
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-08-26 11:18:19 -0400

    delete old console.log

--- src/components/MathBox/MathBoxComponents.js
+++ src/components/MathBox/MathBoxComponents.js
@@ -107,7 +107,6 @@ class AbstractMBC extends React.Component<Props> {
   }
 
   render() {
-    console.log(this.diffProps.updated)
     if (this.mathboxNode) {
       return this.renderChildren()
     }
