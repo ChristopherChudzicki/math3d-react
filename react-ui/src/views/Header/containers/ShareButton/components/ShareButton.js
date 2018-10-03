9050152a291f71cadd4e9b02f4379bce23af465a
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-10-02 22:28:31 -0400

    connect share button to local db

--- react-ui/src/views/Header/containers/ShareButton/components/ShareButton.js
+++ react-ui/src/views/Header/containers/ShareButton/components/ShareButton.js
@@ -1,18 +1,41 @@
 // @flow
 import React, { PureComponent } from 'react'
 import { Button, Popover } from 'antd'
+import { saveGraph } from 'services/api'
+import { dehydrate } from 'store/hydration'
+import randomstring from 'randomstring'
 
 type Props = {
-  onClick: () => void
+  onClick: () => void,
+  state: {}
+}
+type State = {
+  id: ?string
 }
 
-export default class ShareButton extends PureComponent<Props> {
+export default class ShareButton extends PureComponent<Props, State> {
+
+  state = {
+    id: null
+  }
+
+  getId() {
+    return randomstring.generate( { length: 1 } ) +
+      randomstring.generate( { length: 7, charset: 'alphanumeric' } )
+  }
+
+  saveGraph = () => {
+    const dehydrated = dehydrate(this.props.state)
+    const id = this.getId()
+    saveGraph(id, dehydrated)
+    this.setState( { id } )
+  }
 
   renderContent() {
     return (
       <div>
         <pre>
-          cat
+          {this.state.id && `http://localhost:3000/load/${this.state.id}`}
         </pre>
       </div>
     )
@@ -21,7 +44,7 @@ export default class ShareButton extends PureComponent<Props> {
   render() {
     return (
       <Popover placement="bottomRight" title={'Title'} content={this.renderContent()} trigger="click">
-        <Button size='small' type='ghost'>
+        <Button size='small' type='ghost' onClick={this.saveGraph}>
           Share
         </Button>
       </Popover>
