3b79a8c72067e4dd9fa03d486a7a579b16bd47fd
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-10-28 21:54:00 -0400

    return hex color from lighten

--- react-ui/src/utils/colors.js
+++ react-ui/src/utils/colors.js
@@ -5,5 +5,5 @@ export function lighten(colorStr: string, amount: number) {
   const color = Color(colorStr)
   const lightness = color.lightness()
   const increaseBy = amount * (1 - lightness / 100)
-  return color.lighten(increaseBy).toString()
+  return color.lighten(increaseBy).hex()
 }
