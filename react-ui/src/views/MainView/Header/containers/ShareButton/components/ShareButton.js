9fe789b56f29d79156e308a2cb8030bbea496a10
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-11-04 21:21:46 -0500

    add snazzy copy button

--- react-ui/src/views/MainView/Header/containers/ShareButton/components/ShareButton.js
+++ react-ui/src/views/MainView/Header/containers/ShareButton/components/ShareButton.js
@@ -1,22 +1,54 @@
 // @flow
 import React, { PureComponent } from 'react'
-import { Button, Popover, Icon } from 'antd'
+import { Button, Popover, Icon, Input } from 'antd'
 import { saveGraph } from 'services/api'
 import { dehydrate } from 'store/hydration'
 import randomstring from 'randomstring'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import styled, { keyframes } from 'styled-components'
+
+const SharePopoverContainer = styled.div`
+  max-width: 300px;
+  display: flex;
+  align-items: center;
+  flex-direction:column;
+`
+
+const CopyContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const fadeIn = keyframes`
+  from { opacity: 0 }
+  to { opacity: 1 }
+`
+const CopyStatus = styled.div`
+  font-size: 125%;
+  width: 75px;
+  margin: 10px;
+  color: ${props => props.theme.primary[4]};
+  font-weight: strong;
+  visibility: ${props => props.isVisible ? 'visible' : 'hidden'};
+  animation: ${props => props.isVisible ? fadeIn : ''} 100ms linear;
+`
+
+const copyButtonStyle = { margin: '10px' }
 
 type Props = {
   onClick: () => void,
   state: {}
 }
 type State = {
-  id: ?string
+  id: ?string,
+  isCopied: boolean
 }
 
 export default class ShareButton extends PureComponent<Props, State> {
 
   state = {
-    id: null
+    id: null,
+    isCopied: false
   }
 
   getId() {
@@ -31,24 +63,35 @@ export default class ShareButton extends PureComponent<Props, State> {
     this.setState( { id } )
   }
 
+  onCopy = async() => {
+    this.setState( { isCopied: true } )
+  }
+
   renderContent() {
     const url = this.state.id && `https://math3d-react.herokuapp.com/${this.state.id}`
     return (
-      <div style={ { width: 400 } }>
-        <pre>
-          {url}
-        </pre>
+      <SharePopoverContainer>
+        <Input value={url}/>
+        <CopyContainer>
+          <CopyToClipboard text={url} onCopy={this.onCopy}>
+            <Button type='primary' style={copyButtonStyle} >Copy</Button>
+          </CopyToClipboard>
+          <CopyStatus isVisible={this.state.isCopied}>
+            Copied!
+          </CopyStatus>
+        </CopyContainer>
+
         <p>
           <Icon type="warning" theme="outlined" /> This updated version of math3d is in <strong>beta</strong>.
           Graphs saved now may not work in the future.
         </p>
-      </div>
+      </SharePopoverContainer>
     )
   }
 
   render() {
     return (
-      <Popover placement="bottomRight" title={'Share'} content={this.renderContent()} trigger="click">
+      <Popover placement="bottomRight" title={'Share your scene'} content={this.renderContent()} trigger="click">
         <Button size='small' type='ghost' onClick={this.saveGraph}>
           Share
         </Button>
