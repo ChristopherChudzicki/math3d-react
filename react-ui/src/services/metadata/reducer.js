b1b8552012da59f9b46d964f3e42ab8eddc0e96f
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-11-21 10:37:09 -0500

    add title input

--- react-ui/src/services/metadata/reducer.js
+++ react-ui/src/services/metadata/reducer.js
@@ -1,15 +1,36 @@
 // @flow
+import {
+  SET_TITLE, typeof setTitle
+} from './actions'
+import type { ExtractReturn } from 'utils/flow'
 
 type State = {
   versionAtCreation: ?string,
   creationDate: string
 }
 
+type Action = ExtractReturn<setTitle>
+
 const initialState: State = {
+  title: 'Untitled',
   versionAtCreation: process.env.REACT_APP_VERSION,
   creationDate: JSON.stringify(new Date())
 }
 
-export default function(state: State = initialState) {
-  return state
+export default function(state: State = initialState, action: Action) {
+  const { type, payload } = action
+
+  switch (type) {
+
+    case SET_TITLE: {
+      const title = payload.title
+      return { ...state, title }
+    }
+
+    default: {
+      return state
+    }
+
+  }
+
 }
