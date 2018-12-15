d4975839ae29d6b9a89e8529c5ee401c40265034
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-15 15:41:20 -0500

    change initDb to update OR insert

--- server/initDb.js
+++ server/initDb.js
@@ -1,18 +1,31 @@
 const dotenv = require('dotenv')
 const mongodb = require('mongodb')
+const argv = require('minimist')(process.argv.slice(2));
 
 dotenv.config()
 
-const DATABASE_URI = process.env.MONGODB_URI || process.env.LOCAL_MONGODB_URI
 const GRAPH_COLLECTION = 'graph'
 
-const examples = [
-  { "_id": "1", "value": "cat" },
-  { "_id": "2", "value": "dog" },
-  { "_id": "3", "value": "zebra" }
-]
+if (argv.db === 'remote') {
+  DATABASE_URI = process.env.REMOTE_MONGODB_URI
+}
+else if (argv.db === 'local') {
+  DATABASE_URI = process.env.LOCAL_MONGODB_URI
+}
+else {
+  console.log(`Expected 'db=local' or 'db=remote', instead 'db=${argv.db}'`)
+  process.exit()
+}
 
-mongodb.MongoClient.connect(DATABASE_URI, function (err, client) {
+const examples = {
+  1: { a: 'cat', b: 'zebra' },
+  2: { a: 'cat', b: 'wolf' },
+  3: { a: 'lion', b: 'dog' },
+  4: { a: 'horse', b: 'dog' },
+}
+
+const options = { useNewUrlParser: true }
+mongodb.MongoClient.connect(DATABASE_URI, options, function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -24,6 +37,10 @@ mongodb.MongoClient.connect(DATABASE_URI, function (err, client) {
 
   const collection = db.collection(GRAPH_COLLECTION)
 
-  collection.insertMany(examples)
+  const bulk = collection.initializeUnorderedBulkOp();
+  Object.keys(examples).forEach((key) => {
+    bulk.find( { _id : key } ).upsert().update( { $set: examples[key] } );
+  } )
+  bulk.execute();
 
-})
+} )
