d8d2e16f7b1a6e708f3e562b8179965048de859e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-16 14:00:23 -0500

    add examples by looping over examples dir

--- server/initDb.js
+++ server/initDb.js
@@ -1,10 +1,16 @@
 const dotenv = require('dotenv')
 const mongodb = require('mongodb')
 const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
+const promisify = require('util').promisify
+
+const readdir = promisify(fs.readdir)
+const readFile = promisify(fs.readFile)
+
+const dir_path = './server/examples';
+const GRAPH_COLLECTION = 'graph'
 
 dotenv.config()
-
-const GRAPH_COLLECTION = 'graph'
 
 if (argv.db === 'remote') {
   DATABASE_URI = process.env.REMOTE_MONGODB_URI
@@ -17,15 +23,9 @@ else {
   process.exit()
 }
 
-const examples = {
-  1: { a: 'cat', b: 'zebra' },
-  2: { a: 'cat', b: 'wolf' },
-  3: { a: 'lion', b: 'dog' },
-  4: { a: 'horse', b: 'dog' },
-}
-
 const options = { useNewUrlParser: true }
-mongodb.MongoClient.connect(DATABASE_URI, options, function (err, client) {
+mongodb.MongoClient.connect(DATABASE_URI, options, async (err, client) => {
+
   if (err) {
     console.log(err);
     process.exit(1);
@@ -37,9 +37,19 @@ mongodb.MongoClient.connect(DATABASE_URI, options, function (err, client) {
 
   const collection = db.collection(GRAPH_COLLECTION)
 
+  const files = await readdir(dir_path)
+  const contents = await Promise.all( files.map(file => {
+    return readFile(`${dir_path}/${file}`, 'utf8')
+  } ) )
+  const examples = contents.map(content => {
+    return JSON.parse(content)
+  } )
+
   const bulk = collection.initializeUnorderedBulkOp();
-  Object.keys(examples).forEach((key) => {
-    bulk.find( { _id : key } ).upsert().update( { $set: examples[key] } );
+  examples.forEach((data, index) => {
+    const extension = '.json'
+    const _id = files[index].slice(0, -extension.length)
+    bulk.find( { _id } ).upsert().update( { $set: data } );
   } )
   bulk.execute();
 
