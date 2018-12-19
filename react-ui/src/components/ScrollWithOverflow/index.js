583ca035be101100dab17d0420cbdbced831f6fe
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-19 12:43:31 -0500

    prevent overscroll

--- react-ui/src/components/ScrollWithOverflow/index.js
+++ react-ui/src/components/ScrollWithOverflow/index.js
@@ -31,8 +31,7 @@ const ScrollingDiv = styled.div`
   padding-right: 100vw;
   margin-right: -100vw;
   height: 100%;
-  border: 1pt solid red;
-  pointer-events: ${props => props.disablePointer ? 'none' : 'auto'};
+  border: 1pt solid blue;
 `
 
 const ScrollingDivInner = styled.div`
