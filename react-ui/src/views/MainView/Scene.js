9980725deba17ea0db1d81e0675fe59af4d86132
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-19 10:30:06 -0500

    hide overflow

--- react-ui/src/views/MainView/Scene.js
+++ react-ui/src/views/MainView/Scene.js
@@ -1,17 +1,24 @@
 import React, { PureComponent } from 'react'
-import FlexContainer from 'components/FlexContainer'
 import MathBoxContainer from 'containers/MathBoxContainer'
 import MathBoxScene from 'containers/MathBoxScene'
+import styled from 'styled-components'
+
+const SceneBoundary = styled.div`
+  display:flex;
+  height:100%;
+  overflow: hidden;
+  flex: 1;
+`
 
 export default class Math3dScene extends PureComponent {
 
   render() {
     return (
-      <FlexContainer style={ { flex: 1 } } >
+      <SceneBoundary>
         <MathBoxContainer mathboxElement={window.mathboxElement}>
           <MathBoxScene />
         </MathBoxContainer>
-      </FlexContainer>
+      </SceneBoundary>
     )
   }
 
