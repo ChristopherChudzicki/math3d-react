0867a42bfcbbfb0111ca5d8781c46ab71076db5b
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-22 12:45:20 -0500

    add more examples

--- react-ui/src/views/MainView/Examples/data.js
+++ react-ui/src/views/MainView/Examples/data.js
@@ -16,6 +16,42 @@ export const neatExamples: Examples = [
   {
     id: 'sliders_plotrange',
     title: 'Using Sliders to Animate Plot Range'
+  },
+  {
+    id: 'color_and_visibility',
+    title: 'Color and Visibility'
+  },
+  {
+    id: 'vectors',
+    title: 'Vectors and Vector Tails'
+  },
+  {
+    id: 'labels',
+    title: 'Labeling Points, Vectors, and Lines'
+  },
+  {
+    id: 'functions',
+    title: 'Using Functions'
+  },
+  {
+    id: 'derivatives',
+    title: 'diff: Using Derivatives'
+  },
+  {
+    id: 'calculated_visibility',
+    title: 'Controlling Multiple Object Visibility with Toggles'
+  },
+  {
+    id: 'z_bias',
+    title: 'Fine-tuning Visibility with z-bias'
+  },
+  {
+    id: 'tnb',
+    title: 'The TNB Vectors for Parametric Curves'
+  },
+  {
+    id: 'animate_camera',
+    title: 'Animating the Camera Position'
   }
 ]
 
