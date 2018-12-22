51a0a83e803c3e40e2cc557c6af760597e991d62
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-22 15:39:58 -0500

    fix typo

--- react-ui/src/views/MainView/actions.js
+++ react-ui/src/views/MainView/actions.js
@@ -4,8 +4,8 @@ import { loadDehydratedState } from 'store/actions'
 export function loadGraphFromDb(id) {
 
   return async dispatch => {
-    const { dehydrated, timesAcessed = 0 } = await getGraph(id)
-    updateGraph(id, { timesAcessed: timesAcessed + 1 } )
+    const { dehydrated, timesAccessed = 0 } = await getGraph(id)
+    updateGraph(id, { timesAccessed: timesAccessed + 1 } )
     const action = loadDehydratedState(dehydrated)
     return dispatch(action)
   }
