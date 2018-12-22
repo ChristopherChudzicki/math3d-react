059db20ee4eb35546a84f634523ce5942ffced45
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-22 15:36:12 -0500

    update timesAccessed when loading graph

--- react-ui/src/views/MainView/actions.js
+++ react-ui/src/views/MainView/actions.js
@@ -1,10 +1,11 @@
-import { getGraph } from 'services/api'
+import { getGraph, updateGraph } from 'services/api'
 import { loadDehydratedState } from 'store/actions'
 
 export function loadGraphFromDb(id) {
 
   return async dispatch => {
-    const { dehydrated } = await getGraph(id)
+    const { dehydrated, timesAcessed = 0 } = await getGraph(id)
+    updateGraph(id, { timesAcessed: timesAcessed + 1 } )
     const action = loadDehydratedState(dehydrated)
     return dispatch(action)
   }
