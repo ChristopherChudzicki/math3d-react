ce06a52f2aa4b2a9b0afde9ba00e3c268cbe2efa
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-21 20:26:44 -0500

    remove command line options

--- server/initDb.js
+++ server/initDb.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv')
 const mongodb = require('mongodb')
-const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 const promisify = require('util').promisify
 
@@ -12,16 +11,7 @@ const GRAPH_COLLECTION = 'graph'
 
 dotenv.config()
 
-if (argv.db === 'remote') {
-  DATABASE_URI = process.env.REMOTE_MONGODB_URI
-}
-else if (argv.db === 'local') {
-  DATABASE_URI = process.env.LOCAL_MONGODB_URI
-}
-else {
-  console.log(`Expected 'db=local' or 'db=remote', instead 'db=${argv.db}'`)
-  process.exit()
-}
+const DATABASE_URI = process.env.MONGODB_URI || process.env.LOCAL_MONGODB_URI
 
 const options = { useNewUrlParser: true }
 mongodb.MongoClient.connect(DATABASE_URI, options, async (err, client) => {
