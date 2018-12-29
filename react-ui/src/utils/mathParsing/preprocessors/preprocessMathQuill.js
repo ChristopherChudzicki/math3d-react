7a2964fc73a057f820a032d92a3f14a4f3c46bd7
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-28 19:20:04 -0500

    add mod to operatornames

--- react-ui/src/utils/mathParsing/preprocessors/preprocessMathQuill.js
+++ react-ui/src/utils/mathParsing/preprocessors/preprocessMathQuill.js
@@ -17,6 +17,7 @@ import { replaceAll, findClosingBrace } from '../../helpers'
 export default function mathquillToMathJS(fromMQ: string) {
   const replacements = [
     { tex: '\\operatorname{diff}', mathjs: 'diff' },
+    { tex: '\\operatorname{mod}', mathjs: 'mod' },
     { tex: '\\operatorname{unitT}', mathjs: 'unitT' },
     { tex: '\\operatorname{unitN}', mathjs: 'unitN' },
     { tex: '\\operatorname{unitB}', mathjs: 'unitB' },
