f630cbcf1db141743961c81b6cd03f57d7c999e9
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-12-29 09:17:57 -0500

    add temperature color map

--- react-ui/src/constants/colors.js
+++ react-ui/src/constants/colors.js
@@ -16,7 +16,7 @@ export const colors = [
 ]
 
 export const colorMaps = {
-  'rainbow': {
+  rainbow: {
     css: `background: linear-gradient(
       to right,
       hsl(0, 100%, 50%),
@@ -34,10 +34,26 @@ export const colorMaps = {
       return [color.r, color.g, color.b, 1.0]
     }
   },
-  'bluered': {
+  bluered: {
     css: 'background: linear-gradient(to right, blue, red)',
     func: (frac: number) => {
       return [frac, 0, 1 - frac, 1]
+    }
+  },
+  temperature: {
+    css: `background: linear-gradient(
+      to right,
+      hsl(0, 100%, 50%),
+      hsl(60, 100%, 50%),
+      hsl(120, 100%, 50%),
+      hsl(180, 100%, 50%),
+      hsl(240, 100%, 50%)
+    )
+    `,
+    func: (frac: number) => {
+      const color = new THREE.Color(0xffffff)
+      color.setHSL(0.666*(1 - frac), 1, 0.5)
+      return [color.r, color.g, color.b, 1.0]
     }
   }
 }
