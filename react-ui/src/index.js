aeb8d4ad8ccd7df4aea64ae1d5bbcd578b58a060
Chris Chudzicki <christopher.chudzicki@gmail.com>
2019-01-07 23:10:53 -0500

    move react-ga

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -14,6 +14,13 @@ import { ThemeProvider } from 'styled-components'
 
 import { BrowserRouter } from 'react-router-dom'
 
+import ReactGA from 'react-ga'
+// TODO: Replace this with redux-beacon middleware
+ReactGA.initialize('UA-131928751-1', {
+  testMode: process.env.NODE_ENV === 'development'
+} )
+ReactGA.pageview(window.location.pathname + window.location.search)
+
 // run all the code in mockState
 // import './store/mockState'
 
