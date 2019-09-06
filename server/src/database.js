dd48dc3dc89dabca5da593f87632c766533c130d
Chris Chudzicki <christopher.chudzicki@gmail.com>
2019-09-06 19:46:42 -0400

    fix typo

--- server/src/database.js
+++ server/src/database.js
@@ -6,12 +6,6 @@ import { promisify } from 'util'
 const readdir = promisify(fs.readdir)
 const readFile = promisify(fs.readFile)
 
-// To avoid deprecation warnings.
-// https://mongoosejs.com/docs/deprecations.html
-mongoose.set('useNewUrlParser', true)
-mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true)
-
 export const DATABASE_URI = process.env.MONGODB_URI
 export async function seedDb() {
   const examplesDir = './examples'
@@ -26,11 +20,11 @@ export async function seedDb() {
   const extension = '.json'
   const operations = contents.map((content, i) => {
     const _id = files[i].slice(0, -extension.length)
-    const dehyrated = JSON.parse(content)
+    const dehydrated = JSON.parse(content)
     return {
       updateOne: {
         filter: { _id },
-        update: { dehyrated },
+        update: { dehydrated },
         upsert: true
       }
     }
