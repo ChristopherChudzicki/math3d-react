98c5f8618507e4dbd92ea1feaa0d13776063324e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2020-02-29 16:21:35 -0500

    add super simple lastSavedState reducer
    This is intended to store a deep clone of the last saved state.
    On window.beforeunload, we'll diff the current state with the last saved state
    and decide whether to warn the user

---
+++ client/src/store/lastSavedState/actions.js
@@ -0,0 +1,17 @@
+import cloneDeep from 'clone-deep'
+export const SET_SAVED_STATE = 'SET_SAVED_STATE';
+
+function setLastSavedStateAs(state) {
+  return {
+    type: SET_SAVED_STATE,
+    payload: { state },
+  }
+}
+
+export function setLastSavedState() {
+  return (dispatch, getStore) => {
+    const clonedStore = cloneDeep(getStore());
+    console.log(clonedStore)
+    return dispatch(setLastSavedStateAs(clonedStore))
+  }
+}
