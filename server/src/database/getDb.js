6679573aae277b30290c093345b43de42a08024d
Christopher Chudzicki <christopher.chudzicki@gmail.com>
2022-01-17 09:39:26 -0500

    do not use ssl for local db

--- server/src/database/getDb.js
+++ server/src/database/getDb.js
@@ -3,7 +3,10 @@ import pg from 'pg-promise'
 /**
  * This doesn't seem great.
  */
-const DEFAULT_OPTIONS = { ssl: { sslmode: 'require', rejectUnauthorized: false  } };
+const DEFAULT_OPTIONS = 
+  process.env.NODE_ENV === 'development'
+  ? {}
+  : { ssl: { sslmode: 'require', rejectUnauthorized: false  } };
 
 export const getDb = (options) => pg()({
   connectionString: process.env.DATABASE_URL,
